d4b6df6911e790bbe8239cc313a9bc34372da83d
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-06-14 09:16:08 +0700

    Update user.api.js

--- api/user.api.js
+++ api/user.api.js
@@ -5,11 +5,32 @@ exports.list = async(req, res, next)=>{
         let listUser = await md.spModel.find();
 
         if (listUser) {
-            res.status(200).json({
-                msg: 'Lay du lieu thanh cong',
-                data: listUser
-                
+            res.status(200).json(listUser
+              
+            );
+        } else {
+            res.status(204).json({
+                msg: 'Deo co du lieu',
+
             });
+        }
+    } catch (err) {
+        return res.status(err.status).json({
+            msg: err.massage
+        });
+    }
+
+    // res.status(200).json( { msg: 'Danh sách tài khoản'}  );
+ }
+
+ exports.tb_user = async(req, res, next)=>{
+    try {
+        let tbUser = await md.userModel.find();
+
+        if (tbUser) {
+            res.status(200).json(tbUser
+              
+            );
         } else {
             res.status(204).json({
                 msg: 'Deo co du lieu',
