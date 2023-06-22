c89820972273bd4490d365a8276c82b0d096cbe4
NguyenDucDungMc <dungndph26258@fpt.edu.vn>
2023-06-22 10:16:55 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,8 @@ router.get('/chitietsp/:idsp',homeCtrl.chitietsp);
 router.post('/chitietsp/:idsp',homeCtrl.chitietsp);
 router.get('/addTL',homeCtrl.addTL);
 router.post('/addTL',homeCtrl.addTL);
+router.get('/belon',homeCtrl.belon);
+router.post('/belon',homeCtrl.belon);
+router.get('/lonbe',homeCtrl.lonbe);
+router.post('/lonbe',homeCtrl.lonbe);
 module.exports = router;
